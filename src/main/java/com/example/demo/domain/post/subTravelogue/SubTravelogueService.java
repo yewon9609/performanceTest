8c324f72e687687c639257d38d8e611cf42c5a78
yewon.seo <dodnjs1241@naver.com>
2023-05-18 03:50:26 +0900

    feat: service 로직 추가

---
+++ src/main/java/com/example/demo/domain/post/subTravelogue/SubTravelogueService.java
@@ -0,0 +1,47 @@
+package com.example.demo.domain.post.subTravelogue;
+
+import com.example.demo.domain.post.image.dto.TravelPhotoCreateReq;
+import com.example.demo.domain.post.subTravelogue.dto.SubTravelogueCreateReq;
+import com.example.demo.domain.post.subTravelogue.dto.SubTravelogueCreateRes;
+import com.example.demo.domain.post.subTravelogue.entity.SubTravelogue;
+import com.example.demo.domain.post.subTravelogue.repository.SubTravelogueRepository;
+import com.example.demo.domain.post.travelogue.entity.Travelogue;
+import com.example.demo.domain.post.travelogue.service.TravelogueService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class SubTravelogueService {
+
+  private final SubTravelogueRepository subTravelogueRepository;
+  private final TravelogueService travelogueService;
+
+  public SubTravelogueService(SubTravelogueRepository subTravelogueRepository,
+      TravelogueService travelogueService) {
+    this.subTravelogueRepository = subTravelogueRepository;
+    this.travelogueService = travelogueService;
+  }
+
+  @Transactional
+  public SubTravelogueCreateRes save(SubTravelogueCreateReq createReq, Long travelogueId) {
+    Travelogue travelogue = travelogueService.getTravelogue(travelogueId);
+    SubTravelogue subTravelogue = subTravelogueRepository.save(createReq.toSubTravelogue(travelogue));
+    addPhotosTo(subTravelogue, createReq);
+    travelogue.addSubTravelogue(subTravelogue);
+    return new SubTravelogueCreateRes(subTravelogue.getId());
+  }
+
+  private void addPhotosTo(
+      SubTravelogue subTravelogue,
+      SubTravelogueCreateReq subTravelogueCreateReq
+  ) {
+    if (subTravelogue.getPhotos().isEmpty()) {
+      return;
+    }
+    subTravelogue.addPhotos(subTravelogueCreateReq.travelPhotoCreateReqs()
+        .stream()
+        .map(TravelPhotoCreateReq::toEntity)
+        .toList());
+  }
+}
---
+++ src/main/java/com/example/demo/domain/post/travelogue/service/TravelogueService.java
@@ -0,0 +1,60 @@
+package com.example.demo.domain.post.travelogue.service;
+
+import com.example.demo.domain.post.travelogue.dto.TravelogueCreateReq;
+import com.example.demo.domain.post.travelogue.dto.TravelogueCreateRes;
+import com.example.demo.domain.post.travelogue.dto.TravelogueSimpleRes;
+import com.example.demo.domain.post.travelogue.entity.Travelogue;
+import com.example.demo.domain.post.travelogue.repository.TravelogueRepository;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class TravelogueService {
+
+  private final TravelogueRepository travelogueRepository;
+
+  public TravelogueService(TravelogueRepository travelogueRepository) {
+    this.travelogueRepository = travelogueRepository;
+  }
+
+  @Transactional
+  public TravelogueCreateRes save(TravelogueCreateReq createReq) {
+    Travelogue travelogue = travelogueRepository.save(createReq.toTravelogue());
+    Long nights = travelogue.getPeriod().getNights();
+    return TravelogueCreateRes.toDto(travelogue.getId(), nights);
+  }
+
+  public Travelogue getTravelogue(Long travelogueId) {
+    return travelogueRepository.findById(travelogueId)
+        .orElseThrow(() -> new IllegalArgumentException("해당 아이디의 게시글이 없습니다"));
+  }
+
+  public List<TravelogueSimpleRes> getTravelogues(Pageable pageable) {
+    return travelogueRepository.getTravelogues(pageable)
+        .stream()
+        .map(TravelogueSimpleRes::toDto)
+        .toList();
+  }
+
+  public Slice<TravelogueSimpleRes> search(String keyword, Pageable pageable) {
+    List<TravelogueSimpleRes> searchResult = travelogueRepository.search(keyword);
+    boolean hasNext = hasNext(searchResult, pageable);
+    return new SliceImpl<>(searchResult, pageable, hasNext);
+
+  }
+
+  private boolean hasNext(List<TravelogueSimpleRes> results, Pageable pageable) {
+    boolean hasNext = false;
+    if (results.size() > pageable.getPageSize()) {
+      hasNext = true;
+      results.remove(pageable.getPageSize());
+    }
+    return hasNext;
+  }
+
+}
